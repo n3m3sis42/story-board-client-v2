bd16469a2ba2abb23b6a1029d5b4b07ac08ce2ff
n3m3sis42 <n3m3sis42@gmail.com>
2017-09-06 08:07:43 -0400

    still working on delete bug

--- src/reducers/reducer_scenes.js
+++ src/reducers/reducer_scenes.js
@@ -7,7 +7,6 @@ import _ from 'lodash'
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_SCENES:
-      console.log(action.payload.data)
       return _.mapKeys(action.payload.data, 'id')
     case CREATE_SCENE:
     case UPDATE_SCENE:
